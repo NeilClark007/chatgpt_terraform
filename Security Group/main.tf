81eb6351abc2f53480b4fa1418ffecab68580958
NeilClark007 <88084741+NeilClarkAtos@users.noreply.github.com>
2023-02-18 17:37:26 +0000

    security group added

--- Security Group/main.tf
+++ Security Group/main.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   
 }
+
+resource "aws_security_group" "default_access_sg" {
+  name        = "default_access_sg"
+  description = "Default Access Security Group"
+  vpc_id      = "vpc-082a909ef4968bab5"
+  
+  ingress {
+    description  = "Allow SSH from 0.0.0.0/0"
+    from_port    = 22
+    to_port      = 22
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+}
