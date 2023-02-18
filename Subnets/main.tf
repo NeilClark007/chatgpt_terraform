1b315a85fd2c69eb7e98035750c4134af3a599ba
NeilClark007 <88084741+NeilClarkAtos@users.noreply.github.com>
2023-02-18 15:51:49 +0000

    updated with chatGPT code

--- Subnets/main.tf
+++ Subnets/main.tf
@@ -1,3 +1,30 @@
 provider "aws" {
   
 }
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id      = "vpc-082a909ef4968bab5"
+  cidr_block  = "10.10.1.0/24"
+  availability_zone = "eu-west-2a"
+  tags = {
+    Name = "subnet_a"
+  }
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id      = "vpc-082a909ef4968bab5"
+  cidr_block  = "10.10.2.0/24"
+  availability_zone = "eu-west-2b"
+  tags = {
+    Name = "subnet_b"
+  }
+}
+
+resource "aws_subnet" "subnet_c" {
+  vpc_id      = "vpc-082a909ef4968bab5"
+  cidr_block  = "10.10.3.0/24"
+  availability_zone = "eu-west-2c"
+  tags = {
+    Name = "subnet_c"
+  }
+}
